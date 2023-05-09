e269fd027721e690c8d045d72da48d20b1d03629
Jason Nelson <jason.andr.nelson@gmail.com>
2023-05-08 18:43:26 -0700

    renaming layout yml to allow a difference between actual hero cards and their abilities

--- hero_cards/generate-herocards.rb
+++ hero_cards/generate-herocards.rb
@@ -3,19 +3,19 @@ require 'game_icons'
 
 
 if ARGV[0].nil?
-  output = "no argument given, using default, Castle_Defense_Data-Heros.csv\n"
+  output = "no argument given, using default, Castle_Defense_Data-Hero-Archer.csv\n"
   print output
-  data = Squib.csv file: 'Castle_Defense_Data-Hero-Archer.csv'
+  abilityData = Squib.csv file: 'Castle_Defense_Data-Hero-Archer.csv'
   print output
 else
   output = "using argument, " + ARGV[0] + "\n"
   print output
-  data = Squib.csv file: ARGV[0]
+  abilityData = Squib.csv file: ARGV[0]
 end
 
 output = "indexing into Hero Card Name column of given csv\n"
 print output
-something = data['Name']
+something = abilityData['Name']
 output = "off of this index, number of cards is: "
 print output
 sizeOfSomething = something.size
@@ -24,12 +24,12 @@ print output
 output = "\n"
 print output
 
-#data = Squib.csv file: 'Tactile_Tabletop_Data-Level_2_CC.csv'
+#abilityData = Squib.csv file: 'Tactile_Tabletop_Data-Level_2_CC.csv'
 #grabbing icons from https://game-icons.net/
 #using gem game_icons to be able to load them
 
 #width/height/dpi measurements provided by template from BoardGameMaker.com, see poker-size.pdf
-Squib::Deck.new(dpi: 300, width: 750, height: 1050, cards: data['Name'].size, layout: 'herocardlayout.yml')  do
+Squib::Deck.new(dpi: 300, width: 750, height: 1050, cards: abilityData['Name'].size, layout: 'herocardabilitylayout.yml')  do
 
   ## overall card stuff
 
@@ -46,22 +46,27 @@ Squib::Deck.new(dpi: 300, width: 750, height: 1050, cards: data['Name'].size, la
 
   ## output file stuff
 
-  save_png prefix: 'archer_card_'
+  save_png prefix: 'hero_card_'
   #save_pdf trim: 37.5
 end
 
-# Squib::Deck.new(dpi: 300, width: 750, height: 1050, cards: 1, layout: 'consumablecardlayout.yml')  do
+Squib::Deck.new(dpi: 300, width: 750, height: 1050, cards: data['Name'].size, layout: 'herocardabilitylayout.yml')  do
 
-  # ## overall card stuff
+  ## overall card stuff
 
-  # background color: 'white'
-  # rect layout: 'cut'
-  # rect layout: 'backOfCards'
-  # svg data: GameIcons.get('rolling-dices').recolor(fg: 'fff', bg: '000', fg_opacity: 1, bg_opacity: 0).string, layout: 'diceBack'
-  # svg data: GameIcons.get('card-random').recolor(fg: 'fff', bg: '000', fg_opacity: 1, bg_opacity: 0).string, layout: 'cardBack'
-  # svg data: GameIcons.get('two-coins').recolor(fg: 'fff', bg: '000', fg_opacity: 1, bg_opacity: 0).string, layout: 'tokensBack'
-  # ## output file stuff
+  background color: 'white'
+  rect layout: 'cut'
+  rect layout: 'safe'
+  
 
-  # save_png prefix: 'ttcc_BACK'
-  # #save_pdf trim: 37.5
-# end
+  text str: data['Name'], layout: 'ttitle'
+  text str: data['Rules'], layout: 'rrules'
+  text str: "Cost:", layout: 'ccostTitle'
+  text str: data['Cost'], layout: 'ccost'
+  text str: data['Range'], layout: 'rrange'
+
+  ## output file stuff
+
+  save_png prefix: 'hero_card_'
+  #save_pdf trim: 37.5
+end
